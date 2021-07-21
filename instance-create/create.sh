03f13553afb9d80d038641bda5bb2ef37bcafb2e
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-07-21 09:50:51 +0530

    Update create.sh

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 LID=lt-0d7ff25d4b092eda6
 LVER=1
 #COMPONENT=$1
@@ -15,7 +14,7 @@ Instance_Create() {
   INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
   STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[].Instances[].State.Name | xargs)
   if [ -z "${INSTANCE_EXISTS}" -o "$STATE" == "terminated"  ]; then
-    aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Project,Value=ROBOSHOP}]" | jq
+    aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Project,Value=TODO}]" | jq
   else
     echo "Instance ${COMPONENT} already exists"
   fi
@@ -30,7 +29,7 @@ Instance_Create() {
 }
 
 if [ "$1" == "all" ]; then
-  for instance in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment ; do
+  for instance in frontend redis users login todo ; do
     Instance_Create $instance-dev
   done
 else
